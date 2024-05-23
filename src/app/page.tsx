69b85585ea0eacd6b13fc3624b4e1f9b3a34384c
kudanill. <achmad24daniel@gmail.com>
2024-05-23 17:18:01 +0700

    add about section for mobile layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,13 +56,45 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-      <div id="about" className="h-screen bg-sky-700 text-white px-16 pt-2">
-        <div className="p-10">
+      <div
+        id="about"
+        className="h-screen bg-sky-700 text-white px-16 pt-4 md:pt-2">
+        <div className="md:p-10">
+          <div className="md:hidden text-center justify-center items-center content-center space-y-4 pt-20">
+            <Link
+              href="https://www.freepik.com/free-vector/colourful-illustration-programmer-working_5483080.htm#fromView=search&page=1&position=2&uuid=7cf50883-2c87-4923-b511-f015ba52e8ea"
+              target="_blank"
+              aria-label="Image by storyset on Freepik">
+              <Image
+                src="/assets/developer.png"
+                alt="Image by storyset on Freepik"
+                className="rounded-full object-cover"
+                data-aos="fade-up"
+                height={500}
+                width={500}
+                loading="lazy"
+              />
+            </Link>
+            <div>
+              <h1 className="font-poppins font-semibold text-3xl">About me</h1>
+              <p className="font-poppins pt-6 text-left">
+                {
+                  "Hi, I'm Achmad Daniel Syahputra, call me 'Daniel', from Indonesia. I'm a mobile developer and also a web developer. Started my coding journey in 2020."
+                }
+              </p>
+              <p className="font-poppins pt-4 text-left">
+                I want to be a professional in a company that offers a positive
+                atmosphere to explore technology and its application for the
+                greater benefit of society in the business world.
+              </p>
+            </div>
+            <div className="pt-6">
+              <Button text="Next" color="border-2 border-white" url="#skills" />
+            </div>
+          </div>
           <div className="hidden md:flex justify-center items-center pt-16 content-center">
             <div>
-              <h1 className="text-2xl font-poppins font-semibold md:text-3xl">
-                About me
-              </h1>
+              <h1 className="font-poppins font-semibold text-3xl">About me</h1>
               <p className="font-poppins max-w-xl pt-6">
                 {
                   "Hi, I'm Achmad Daniel Syahputra, call me 'Daniel', from Indonesia. I'm a mobile developer and also a web developer. Started my coding journey in 2020."
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,23 @@
+import Link from "next/link";
+
 export default function Button(props: any) {
   return (
-    <div
-      aria-label={props.text}
-      className={`rounded-xl mr-2 hover:drop-shadow ${
-        props.color ?? "bg-sky-700 md:bg-gray-300 hover:bg-sky-700 duration-300"
-      }`}>
-      <p
-        className={`px-6 py-3 font-poppins font-medium text-center hover:drop-shadow ${
-          props.color
-            ? "text-white"
-            : "text-white md:text-neutral-700 hover:text-white duration-300"
+    <Link href={props.url ?? ""} aria-label={props.text}>
+      <div
+        aria-label={props.text}
+        className={`rounded-xl mr-2 hover:drop-shadow ${
+          props.color ??
+          "bg-sky-700 md:bg-gray-300 hover:bg-sky-700 duration-300"
         }`}>
-        <a href={props.url ?? ""} aria-label={props.text}>
+        <p
+          className={`px-6 py-3 font-poppins font-medium text-center hover:drop-shadow ${
+            props.color
+              ? "text-white"
+              : "text-white md:text-neutral-700 hover:text-white duration-300"
+          }`}>
           {props.text ?? ""}
-        </a>
-      </p>
-    </div>
+        </p>
+      </div>
+    </Link>
   );
 }
